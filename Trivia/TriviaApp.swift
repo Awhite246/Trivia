adae26d83bb7dd0f47ed08579218894e769c9eae
Alistiar White <awhite@bsd220.org>
2023-01-11 09:38:16 -0600

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Alistair White on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
